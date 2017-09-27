5c3497dfe909fff08077065174fa5147b08dcce7
Michal Karas <zoliqe@gmail.com>
2017-09-27 12:01:01 +0200

    fix for SDK 1.20+

--- lib/serial_port.dart
+++ lib/serial_port.dart
@@ -17,7 +17,7 @@ library serial_port;
 import 'dart:async';
 import 'dart:isolate';
 import 'dart:io';
-import 'dart-ext:src/serial_port';
+import 'dart-ext:serial_port';
 
 part 'src/serial_port_impl.dart';
 part 'src/helpers.dart';
